00952888a37b9c0aac687aa4469db4218f2ce360
Antony <vijaykerolin2006@gmail.com>
2020-08-02 23:36:51 +0530

    My First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Footer() {
+  return (
+    <div className='nav bg-primary footer relative'>
+      <div className='container'>
+        <div className='row'>
+          <div className='content'>
+            <div className='text-center '>developed by : Allocious</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
